b152804996b29e785a71d102d711e183edee7c69
Anjalisoni99-byte <64090095+Anjalisoni99-byte@users.noreply.github.com>
2022-03-21 17:58:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+let currDate= new Date(2022,3,21,6);
+currDate=currDate.getHours();
+let greetingText = "";
+const cssStyle={};
+
+if (currDate>=1 && currDate< 12) {
+  greetingText = "Good Morning"; 
+  cssStyle.color='Red';
+} else if (currDate>=12 && currDate< 19) {
+  greetingText = "Good Afternoon"; 
+  cssStyle.color='Blue';
+} else {
+  greetingText = "Good Evening"; 
+  cssStyle.color='Green';
+}
+ReactDOM.render(
+  <React.Fragment>
+  <h1>Hi, <span style={cssStyle}>{greetingText}</span></h1>
+  </React.Fragment>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
